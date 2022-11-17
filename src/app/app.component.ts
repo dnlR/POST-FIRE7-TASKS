8f35b3cc5228a56cded87e78b2bb6b611332155b
Daniel <romerodaniel@protonmail.com>
2022-11-17 15:36:33 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { Task } from './task/task';
+import { CdkDragDrop, transferArrayItem } from '@angular/cdk/drag-drop';
+import { MatDialog } from '@angular/material/dialog';
+import { TaskDialogComponent } from './task-dialog/task-dialog.component';
+import { TaskDialogResult } from './task-dialog/task-dialog.component';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,70 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'kanban-fire';
+
+  constructor(private dialog: MatDialog, private readonly db: AngularFirestore) { }
+
+  newTask(): void {
+    const dialogRef = this.dialog.open(TaskDialogComponent, {
+      width: '270px',
+      data: {
+        task: {},
+      },
+    });
+    dialogRef
+      .afterClosed()
+      .subscribe((result: TaskDialogResult | undefined) => {
+        if (!result) {
+          return;
+        }
+        this.db.collection('todo').add(result.task);
+      });
+  }
+
+  todo = this.db.collection('todo').valueChanges({ idField: 'id' }) as Observable<Task[]>;
+  inProgress = this.db.collection('inProgress').valueChanges({ idField: 'id' }) as Observable<Task[]>;
+  done = this.db.collection('done').valueChanges({ idField: 'id' }) as Observable<Task[]>;
+
+  drop(event: CdkDragDrop<Task[] | null>): void {
+    if (event.previousContainer === event.container) {
+      return;
+    }
+    if (!event.previousContainer.data || !event.container.data) {
+      return;
+    }
+    const item = event.previousContainer.data[event.previousIndex];
+    this.db.firestore.runTransaction(() => {
+      const promise = Promise.all([
+        this.db.collection(event.previousContainer.id).doc(item.id).delete(),
+        this.db.collection(event.container.id).add(item),
+      ]);
+      return promise;
+    });
+    transferArrayItem(
+      event.previousContainer.data,
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex
+    );
+  }
+
+  editTask(list: 'done' | 'todo' | 'inProgress', task: Task): void {
+    const dialogRef = this.dialog.open(TaskDialogComponent, {
+      width: '270px',
+      data: {
+        task,
+        enableDelete: true,
+      },
+    });
+    dialogRef.afterClosed().subscribe((result: TaskDialogResult | undefined) => {
+      if (!result) {
+        return;
+      }
+      if (result.delete) {
+        this.db.collection(list).doc(task.id).delete();
+      } else {
+        this.db.collection(list).doc(task.id).update(task);
+      }
+    });
+  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'kanbanfire-5afb5',
+    appId: '1:114998454633:web:e0ddb8c3b9146ed15235f8',
+    storageBucket: 'kanbanfire-5afb5.appspot.com',
+    apiKey: 'AIzaSyDVa0xlGt9xGK_p1HpZ7nGVVeiNeWROsx8',
+    authDomain: 'kanbanfire-5afb5.firebaseapp.com',
+    messagingSenderId: '114998454633',
+    measurementId: 'G-4381S1CZFX',
+  },
   production: true
 };
